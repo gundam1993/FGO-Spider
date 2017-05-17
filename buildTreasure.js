640718f36061536785fb8b8facadab5cd8be0071
gundam1993 <gundam1993@gmail.com>
2017-05-17 10:57:16 +0800

    build treasure data from servant table

--- buildTreasure.js
+++ buildTreasure.js
@@ -1,1 +1,29 @@
 const Servant = require('./models/servant.js');
+const Treasure = require('./models/treasure.js');
+
+const buildTreasure = async () => {
+  let servants = await Servant.findAll()
+  const len = servants.length
+  for (var i = 0; i < len; i++) {
+    let sv = servants[i]
+    console.log(sv)
+    if (sv.treasureName === '') {
+      continue
+    } else {
+      let t = await Treasure.create({
+        id: sv.id,
+        servantId: sv.id,
+        name: sv.treasureName,
+        prop: sv.treasureProp,
+        rank: sv.treasureLv,
+        kind: sv.treasureType,
+        effect: sv.treasureEffect,
+        textArr: sv.treasureTextArr,
+        numArr: sv.treasureNumArr
+      })
+      console.log(t)
+    }
+  }
+};
+
+buildTreasure();
